9339e26552af048ef586645248804de663af80ed
petekostrytsya <118105720+petekostrytsya@users.noreply.github.com>
2023-01-25 15:00:17 +0000

    Deploying to gh-pages from @ petekostrytsya/DevHunt@841338b8aa99fc901617bdb0645ae9376fae1ea6 🚀

---
+++ index.1114df27.js
@@ -0,0 +1,2 @@
+$("document").ready((function(){$("#button").on("click",(function(){$(".modal-send-form-input .rfield").each((function(){""!=$(this).val()?$(this).removeClass("empty_field"):$(this).addClass("empty_field")}))}))}));
+//# sourceMappingURL=index.1114df27.js.map
